8b8334ba2818ee1b152e9045bc05a24496700f84
MinhWalker <hcm9113@gmail.com>
2020-02-06 04:33:58 +0700

    add readme content how to use convertByte

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 ###Setup 
 clone source form [github](https://github.com/MinhWalker/IR_manager)
 
+#convertByte (api: ir.renderChecksum() )
+-input: (opt, 11 first byte) . EX: ir.renderChecksum([opt], [11 byte calculated]);
--- test.js
+++ test.js
@@ -7,7 +7,8 @@ var data_prm = data.prm;
 
 var test = ir.getIR_unfix(data_prm);
 console.log(ir.getArrByte(test, [27,1,3,0]));
+console.log(ir.base64toHEX("AAIBAQANAAMB="));
 // console.log(ir.renderChecksum([129,0,0,138,0,11,135,1,1,136,2,4,7,2,3,132,4,5,137,5,4,133,6,1], ir.converArrHexToDec(ir.getArrByte(test, [27,1,3,0]))));
-console.log(ir.checkSum(ir.converArrHexToDec(ir.getArrByte(test, [27,1,3,0]))));
+// console.log(ir.checkSum(ir.converArrHexToDec(ir.getArrByte(test, [27,1,3,0]))));
 // console.log(ir.base64toHEX("gQAAigALhwEBiAIEBwIDhAQPiQUEhQYB"));
 
